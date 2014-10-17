baf1788a2249aad57ce88bd11cf4457f57d92f52
Jon TURNEY <jon.turney@dronecode.org.uk>
2014-10-17 11:40:00 +0100

    Rewrite symsrv-fetch.py to use pdb_downloader and dump_syms and to install the symbol file
    - Add logging for all the reasons why we aren't fetching a file
    - Take note of exit status of dump_syms
    - Don't fail if paths already exist
    - Report count of pdbs for which conversion failed
    - Remove config.py

--- config.py
+++
@@ -1,4 +0,0 @@
-# Read-only path to examine for existing symbols
-read_only_symbol_path='readonly'
-# Temporary directory to store symbols in locally
-temp_dir='temp'
--- symsrv-fetch.py
+++ symsrv-fetch.py
@@ -1,40 +1,37 @@
 #!/usr/bin/env python
 #
-# This script will read a CSV of modules from Socorro, and try to retrieve
-# missing symbols from Microsoft's symbol server. It honors a blacklist
-# (blacklist.txt) of symbols that are known to be from our applications,
-# and it maintains its own list of symbols that the MS symbol server
-# doesn't have (skiplist.txt).
+# This script will read a CSV of modules from fetchlist.txt, and tries to
+# retrieve missing symbols from Microsoft's symbol server. It honours a
+# blacklist (blacklist.txt) of symbols that are known to be from our
+# applications, and it maintains it's own list of symbols that the MS symbol
+# server doesn't have (skiplist.txt).
 #
 # The script must have installed alongside it:
-# * msdia80.dll (from the DIA SDK, installed with Visual C++ 8)
-# * dbghelp.dll (from WinDBG)
-# * symsrv.dll  (also from WinDBG)
-# * symsrv.yes  (a zero-byte file that indicates that you've accepted
-#                the Microsoft symbol server EULA)
-# * config.py   (create this from the template in config.py.in)
+# * pdb_downloader.py
 #
 # The script also depends on having write access to the directory it is
 # installed in, to write the skiplist text file.
 #
-# Finally, you must have 'zip' (Info-Zip), 'scp', and 'ssh' available in %PATH%.
+# Finally, you must have 'dump_syms' and 'cabextract' available in %PATH%.
 
 from __future__ import with_statement
-import config
 import sys
 import os
 import time, datetime
 import subprocess
 import StringIO
-import gzip
 import shutil
 import ctypes
 import logging
 from collections import defaultdict
 from tempfile import mkdtemp
 
-# Just hardcoded here
-MICROSOFT_SYMBOL_SERVER = "http://msdl.microsoft.com/download/symbols"
+# Temporary directory to store symbols in locally
+temp_dir='temp'
+# Path to store symbols in
+symbol_path='symbols'
+# Path to store pdbs in
+pdb_path='pdb'
 
 thisdir = os.path.dirname(__file__)
 
@@ -118,12 +115,12 @@ try:
       log.debug("%s %s" % (pdb, uuid))
       modules[pdb].add(uuid)
       modulescount += 1
+    else:
+      log.debug("%s/%s not a PDB or no UUID" % (pdb, uuid))
 except:
   log.exception("Error reading fetch list")
   sys.exit(1)
 log.debug("Fetchlist contains %d items" % modulescount)
-
-symbol_path = mkdtemp(dir=config.temp_dir)
 
 log.debug("Fetching symbols")
 total = sum(len(ids) for ids in modules.values())
@@ -132,7 +129,9 @@ blacklist_count = 0
 skiplist_count = 0
 existing_count = 0
 not_found_count = 0
-file_index = []
+conversion_failed = 0
+converted_count = 0
+
 # Now try to fetch all the unknown modules from the symbol server
 for filename, ids in modules.iteritems():
   # Sometimes we get non-ascii in here. This is definitely not
@@ -140,47 +139,39 @@ for filename, ids in modules.iteritems():
   filename = filename.encode('ascii', 'replace')
 
   if filename.lower() in blacklist:
-    # This is one of our our debug files from Firefox/Thunderbird/etc
+    log.debug("%s on blacklist", filename)
     current += len(ids)
     blacklist_count += len(ids)
     continue
+
   for id in ids:
     current += 1
     if verbose:
-      sys.stdout.write("[%6d/%6d] %3d%% %-20s\r" % (current, total,
-                                                    int(100 * current / total),
-                                                    filename[:20]))
+      sys.stdout.write("[%6d/%6d] %3d%% %s\n" % (current, total,
+                                                 int(100 * current / total),
+                                                 filename))
     if id in skiplist and skiplist[id] == filename.lower():
       # We've asked the symbol server previously about this, so skip it.
       log.debug("%s/%s already in skiplist", filename, id)
       skiplist_count += 1
       continue
-    rel_path = os.path.join(filename, id,
+
+    sym_file = os.path.join(symbol_path, filename, id,
                             filename.replace(".pdb","") + ".sym")
-    sym_file = os.path.join(symbol_path, rel_path)
+    pdb_file = os.path.join(pdb_path, filename, id, filename)
+
     if os.path.exists(sym_file):
       # We already have this symbol
       log.debug("%s/%s already present", filename, id)
       existing_count += 1
       continue
-    if config.read_only_symbol_path != '' and \
-       os.path.exists(os.path.join(config.read_only_symbol_path, filename, id,
-                                   filename.replace(".pdb","") + ".sym")):
-      # We already have this symbol
-      log.debug("%s/%s already present", filename, id)
-      existing_count += 1
-      continue
+
     # Not in the blacklist, skiplist, and we don't already have it, so
     # ask the symbol server for it.
-    # This expects that symsrv_convert.exe and all its dependencies
-    # are in the current directory.
-    #TODO: make symsrv_convert write to stdout, build zip using ZipFile
-    symsrv_convert = os.path.join(thisdir, ".", "symsrv_convert.exe")
-    proc = subprocess.Popen([symsrv_convert,
-                             MICROSOFT_SYMBOL_SERVER,
-                             symbol_path,
-                             filename,
-                             id],
+    pdb_downloader = os.path.join(thisdir, ".", "pdb_downloader.py")
+    proc = subprocess.Popen([pdb_downloader,
+                             "--file", filename,
+                             "--uuid", id],
                             stdout = subprocess.PIPE,
                             stderr = subprocess.STDOUT)
     # kind of lame, want to prevent it from running too long
@@ -197,35 +188,43 @@ for filename, ids in modules.iteritems():
       # Don't skiplist this symbol if we've previously downloaded
       # other symbol versions for the same file. It's likely we'll
       # be able to download it at some point
-      if not (os.path.exists(os.path.join(symbol_path, filename)) or
-              os.path.exists(os.path.join(config.read_only_symbol_path, filename))):
+      if not (os.path.exists(os.path.join(symbol_path, filename))):
         log.debug("Couldn't fetch %s/%s, adding to skiplist", filename, id)
         skiplist[id] = filename
       else:
         log.debug("Couldn't fetch %s/%s, but not skiplisting", filename, id)
       convert_output = proc.stdout.read().strip()
-      log.debug("symsrv_convert.exe output: '%s'", convert_output)
+      log.debug("pdb_downloader output: '%s'", convert_output)
+
+    if not os.path.exists(filename):
+      log.debug("Failed to download %s/%s", filename, id)
+      continue
+
+    log.debug("Successfully downloaded %s/%s", filename, id)
+
+    if not os.path.exists(os.path.join(pdb_path, filename, id)):
+      os.makedirs(os.path.join(pdb_path, filename, id))
+    shutil.move(filename, pdb_file)
+
+    if not os.path.exists(os.path.join(symbol_path, filename, id)):
+      os.makedirs(os.path.join(symbol_path, filename, id))
+    if os.system("/wip/dump_syms/dump_syms %s >%s" % (pdb_file, sym_file)) != 0:
+      os.remove(sym_file)
+      log.debug("Failed to convert %s/%s", filename, id)
+      conversion_failed = +1
+
     if os.path.exists(sym_file):
-      log.debug("Successfully downloaded %s/%s", filename, id)
-      file_index.append(rel_path.replace("\\", "/"))
+      log.debug("Successfully converted %s/%s", filename, id)
+      converted_count += 1
 
 if verbose:
   sys.stdout.write("\n")
 
-if not file_index:
-  log.info("No symbols downloaded: %d considered, %d already present, %d in blacklist, %d skipped, %d not found"
-           % (total, existing_count, blacklist_count, skiplist_count, not_found_count))
-  write_skiplist()
-  sys.exit(0)
-
-# Install the symbol files
-for filename in file_index:
-  os.system("install-symbols %s" % (os.path.join(symbol_path, filename)))
-
-shutil.rmtree(symbol_path, True)
+log.info("%d considered, %d already present, %d in blacklist, %d skipped, %d not found, %d conversion failed"
+         % (total, existing_count, blacklist_count, skiplist_count, not_found_count, conversion_failed))
 
 # Write out our new skip list
 write_skiplist()
 
-log.info("Converted and installed %d symbol files" % len(file_index))
+log.info("Converted and installed %d symbol files" % converted_count)
 log.info("Finished, exiting")
