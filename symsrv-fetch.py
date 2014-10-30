8981505161334fe42d30bf86d7bab8f0b3b566c8
Jon TURNEY <jon.turney@dronecode.org.uk>
2014-10-30 14:04:47 +0000

    Rewrite symsrv-fetch.py to use pdb_fetch

--- symsrv-fetch.py
+++ symsrv-fetch.py
@@ -3,16 +3,10 @@
 # This script will read a CSV of modules from fetchlist.txt, and tries to
 # retrieve missing symbols from Microsoft's symbol server. It honours a
 # blacklist (blacklist.txt) of symbols that are known to be from our
-# applications, and it maintains it's own list of symbols that the MS symbol
-# server doesn't have (skiplist.txt).
+# applications.
 #
-# The script must have installed alongside it:
-# * pdb_downloader.py
-#
-# The script also depends on having write access to the directory it is
-# installed in, to write the skiplist text file.
-#
-# Finally, you must have 'dump_syms' and 'cabextract' available in %PATH%.
+# You must have 'pdb_fetch' (which requires 'cabextract') and
+# 'dump_syms' available in PATH.
 
 from __future__ import with_statement
 import sys
@@ -34,14 +28,6 @@ symbol_path='symbols'
 pdb_path='pdb'
 
 thisdir = os.path.dirname(__file__)
-
-def write_skiplist():
-  try:
-    with open(os.path.join(thisdir, 'skiplist.txt'), 'w') as sf:
-      for (debug_id,debug_file) in skiplist.iteritems():
-          sf.write("%s %s\n" % (debug_id, debug_file))
-  except IOError:
-    log.exception("Error writing skiplist.txt")
 
 verbose = False
 if len(sys.argv) > 1 and sys.argv[1] == "-v":
@@ -80,29 +66,9 @@ except IOError:
   pass
 log.debug("Blacklist contains %d items" % len(blacklist))
 
-# Symbols that we've asked for in the past unsuccessfully
-skiplist={}
-skipcount = 0
-try:
-  sf = file(os.path.join(thisdir, 'skiplist.txt'), 'r')
-  for line in sf:
-      line = line.strip()
-      if line == '':
-          continue
-      s = line.split(None, 1)
-      if len(s) != 2:
-        continue
-      (debug_id, debug_file) = s
-      skiplist[debug_id] = debug_file.lower()
-      skipcount += 1
-  sf.close()
-except IOError:
-  pass
-log.debug("Skiplist contains %d items" % skipcount)
-
 modules = defaultdict(set)
 modulescount = 0
-log.debug("Loading module list...")
+log.debug("Loading fetchlist...")
 try:
   mf = file(os.path.join(thisdir, 'fetchlist.txt'), 'r')
   for line in mf:
@@ -126,7 +92,6 @@ log.debug("Fetching symbols")
 total = sum(len(ids) for ids in modules.values())
 current = 0
 blacklist_count = 0
-skiplist_count = 0
 existing_count = 0
 not_found_count = 0
 conversion_failed = 0
@@ -150,15 +115,9 @@ for filename, ids in modules.iteritems():
       sys.stdout.write("[%6d/%6d] %3d%% %s\n" % (current, total,
                                                  int(100 * current / total),
                                                  filename))
-    if id in skiplist and skiplist[id] == filename.lower():
-      # We've asked the symbol server previously about this, so skip it.
-      log.debug("%s/%s already in skiplist", filename, id)
-      skiplist_count += 1
-      continue
 
     sym_file = os.path.join(symbol_path, filename, id,
                             filename.replace(".pdb","") + ".sym")
-    pdb_file = os.path.join(pdb_path, filename, id, filename)
 
     if os.path.exists(sym_file):
       # We already have this symbol
@@ -166,45 +125,25 @@ for filename, ids in modules.iteritems():
       existing_count += 1
       continue
 
-    # Not in the blacklist, skiplist, and we don't already have it, so
-    # ask the symbol server for it.
-    pdb_downloader = os.path.join(thisdir, ".", "pdb_downloader.py")
+    # Not in the blacklist, and we don't already have it, so ask the symbol
+    # server for it.
+    pdb_downloader = os.path.join(thisdir, ".", "pdb_fetch")
     proc = subprocess.Popen([pdb_downloader,
                              "--file", filename,
-                             "--uuid", id],
+                             "--uuid", id,
+                             "--cache", pdb_path],
                             stdout = subprocess.PIPE,
                             stderr = subprocess.STDOUT)
-    # kind of lame, want to prevent it from running too long
-    start = time.time()
-    # 30 seconds should be more than enough time
-    while proc.poll() is None and (time.time() - start) < 30:
-      time.sleep(1)
-    if proc.poll() is None:
-      # kill it, it's been too long
-      log.debug("Timed out downloading %s/%s", filename, id)
-      ctypes.windll.kernel32.TerminateProcess(int(proc._handle), -1)
-    elif proc.returncode != 0:
-      not_found_count += 1
-      # Don't skiplist this symbol if we've previously downloaded
-      # other symbol versions for the same file. It's likely we'll
-      # be able to download it at some point
-      if not (os.path.exists(os.path.join(symbol_path, filename))):
-        log.debug("Couldn't fetch %s/%s, adding to skiplist", filename, id)
-        skiplist[id] = filename
-      else:
-        log.debug("Couldn't fetch %s/%s, but not skiplisting", filename, id)
-      convert_output = proc.stdout.read().strip()
-      log.debug("pdb_downloader output: '%s'", convert_output)
 
-    if not os.path.exists(filename):
-      log.debug("Failed to download %s/%s", filename, id)
+    if proc.returncode != 0:
+      not_found_count += 1
+      log.debug("pdb_fetch failed for %s/%s", filename, id)
       continue
 
-    log.debug("Successfully downloaded %s/%s", filename, id)
+    log.debug("Successfully fetched %s/%s", filename, id)
 
-    if not os.path.exists(os.path.join(pdb_path, filename, id)):
-      os.makedirs(os.path.join(pdb_path, filename, id))
-    shutil.move(filename, pdb_file)
+    pdb_file = proc.stdout.read().strip()
+    log.debug("pdb_fetch output: '%s'", pdb_file)
 
     if not os.path.exists(os.path.join(symbol_path, filename, id)):
       os.makedirs(os.path.join(symbol_path, filename, id))
@@ -220,11 +159,8 @@ for filename, ids in modules.iteritems():
 if verbose:
   sys.stdout.write("\n")
 
-log.info("%d considered, %d already present, %d in blacklist, %d skipped, %d not found, %d conversion failed"
-         % (total, existing_count, blacklist_count, skiplist_count, not_found_count, conversion_failed))
-
-# Write out our new skip list
-write_skiplist()
+log.info("%d considered, %d already present, %d in blacklist, %d not found, %d conversion failed"
+         % (total, existing_count, blacklist_count, not_found_count, conversion_failed))
 
 log.info("Converted and installed %d symbol files" % converted_count)
 log.info("Finished, exiting")
